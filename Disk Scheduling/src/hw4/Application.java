51ae3b4894ec6bdd5217114989c0f10f854c9620
Chuma Okoro <chuma.okoro76@gmail.com>
2018-12-03 11:30:33 -0500

    Create Java Project

---
+++ Disk Scheduling/src/hw4/Application.java
@@ -0,0 +1,25 @@
+package hw4;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("These are the 3 Disk Scheduling Algorithms");
+		
+
+	}
+	
+	static void fcfs() {
+		
+	}
+	
+	static void sstf() {
+		
+	}
+
+	static void scan() {
+		
+	}
+	
+
+}
